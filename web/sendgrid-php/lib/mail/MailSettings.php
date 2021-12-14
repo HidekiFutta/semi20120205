fbe609f0c0dd6685ee18698bbd520a32a0699fc7
daihougi <hima71f@gmail.com>
2021-12-14 11:51:00 +0900

    add new file

--- web/sendgrid-php/lib/mail/MailSettings.php
+++ web/sendgrid-php/lib/mail/MailSettings.php
@@ -252,6 +252,7 @@ class MailSettings implements \JsonSerializable
      *
      * @return null|array
      */
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return array_filter(
