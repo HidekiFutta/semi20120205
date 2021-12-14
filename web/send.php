722c7b2605bdd0d3e8e402df50804288c5b46f81
daihougi <hima71f@gmail.com>
2021-12-14 14:37:07 +0900

    add new file

--- web/send.php
+++ web/send.php
@@ -134,7 +134,7 @@ $email = new \SendGrid\Mail\Mail();
     try {
       $response = $sendgrid->send($email);
       //print $response->statusCode() . "\n";
-      print_r($response);//->headers());
+      //print_r($response)->headers());
       //print $response->body() . "\n";    
     } catch (Exception $e) {
       echo 'Caught exception: '. $e->getMessage() ."\n";
